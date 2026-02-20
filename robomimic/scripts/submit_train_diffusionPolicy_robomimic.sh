f8aa94db3dc0bab64ffefef0a7e015adfea2cbc7
Yutai Zhou <yutaizh@gmail.com>
2026-02-20 07:34:53 +0000

    script bmp

--- robomimic/scripts/submit_train_diffusionPolicy_robomimic.sh
+++ robomimic/scripts/submit_train_diffusionPolicy_robomimic.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#SBATCH --job-name=train_dp_robomimic
+#SBATCH --array=0-1
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
@@ -12,7 +14,20 @@ source ~/miniconda3/etc/profile.d/conda.sh
 cd /scr/yutaizho/code/p-infOPE/inf_ope/src/inf_ope/external/robomimic_infope
 conda activate ope2
 
+DATASETS=(
+    # "/scr/shared/datasets/robomimic0.5/lift/ph/image_v15.hdf5"
+    # "/scr/shared/datasets/robomimic0.5/lift/mh/image_v15.hdf5"
+    "/scr/shared/datasets/robomimic0.5/can/ph/image_v15.hdf5"
+    "/scr/shared/datasets/robomimic0.5/can/mh/image_v15.hdf5"
+    # "/scr/shared/datasets/robomimic0.5/square/ph/image_v15.hdf5"
+    # "/scr/shared/datasets/robomimic0.5/square/mh/image_v15.hdf5"
+    # "/scr/shared/datasets/robomimic0.5/transport/ph/image_v15.hdf5"
+    # "/scr/shared/datasets/robomimic0.5/transport/mh/image_v15.hdf5"
+    # "/scr/shared/datasets/robomimic0.5/tool_hang/ph/image_v15.hdf5"
+)
+
+dataset=${DATASETS[$SLURM_ARRAY_TASK_ID]}
 
 python robomimic/scripts/train_policy_robomimic.py \
     --config robomimic/exps/templates/diffusion_policy_image.json \
-    --dataset /scr/shared/datasets/robomimic0.5/lift/mh/image_v15.hdf5
+    --dataset $dataset
