98c66304560c3cbf9adc7da678002cd6b592b04c
Yutai Zhou <yutaizh@gmail.com>
2026-04-01 21:03:14 +0000

    train script

--- robomimic/scripts/submit_train_diffusionPolicy_robomimic.sh
+++ robomimic/scripts/submit_train_diffusionPolicy_robomimic.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=train_dp_robomimic
-#SBATCH --array=0-1
+#SBATCH --array=0-3%2
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=24:00:00
+#SBATCH --time=40:00:00
 #SBATCH --output=logs_slurm/train_diffusionPolicy_%j.log
 #SBATCH --error=/dev/null
 
@@ -19,8 +19,8 @@ DATASETS=(
     # "/scr/shared/datasets/robomimic0.5/lift/mh/image_v15.hdf5"
     "/scr/shared/datasets/robomimic0.5/can/ph/image_v15.hdf5"
     "/scr/shared/datasets/robomimic0.5/can/mh/image_v15.hdf5"
-    # "/scr/shared/datasets/robomimic0.5/square/ph/image_v15.hdf5"
-    # "/scr/shared/datasets/robomimic0.5/square/mh/image_v15.hdf5"
+    "/scr/shared/datasets/robomimic0.5/square/ph/image_v15.hdf5"
+    "/scr/shared/datasets/robomimic0.5/square/mh/image_v15.hdf5"
     # "/scr/shared/datasets/robomimic0.5/transport/ph/image_v15.hdf5"
     # "/scr/shared/datasets/robomimic0.5/transport/mh/image_v15.hdf5"
     # "/scr/shared/datasets/robomimic0.5/tool_hang/ph/image_v15.hdf5"
